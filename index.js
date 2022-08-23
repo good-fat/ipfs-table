5671ea34dc4c3899b7ef86f75a0a85e009eb516d
dld <web2018.com@gmail.con>
2022-08-23 17:25:58 +0800

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: zhang yu meng
+ * @Date: 2022-08-23 17:18:48
+ * @LastEditors: dld web2018.com@gmail.con
+ * @LastEditTime: 2022-08-23 17:18:53
+ * @FilePath: \ipfs-table\index.js
+ * @Description:
+ *
+ * Copyright (c) 2022 by dld web2018.com@gmail.con, All Rights Reserved.
+ */
+// gettime
+const getTime = () => {
+  return new Date().getTime();
+}
