0da286fadb90ff9d87b6dcdaf358b8d9e458ec0e
dld <web2018.com@gmail.con>
2022-08-26 09:51:01 +0800

    提交mock数据

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
  * @Author: zhang yu meng
  * @Date: 2022-08-23 17:18:48
  * @LastEditors: dld web2018.com@gmail.con
- * @LastEditTime: 2022-08-23 17:18:53
+ * @LastEditTime: 2022-08-24 08:50:30
  * @FilePath: \ipfs-table\index.js
  * @Description:
  *
  * Copyright (c) 2022 by dld web2018.com@gmail.con, All Rights Reserved.
  */
 // gettime
-const getTime = () => {
-  return new Date().getTime();
+/**
+ * @description: 
+ * @param {*} 
+ * @return {*}
+ */
+const getTime = (a) => {
+  return new Date().getTime()
 }
